f6e9520fad174459e8d374462a6905644872b52c
Giuseppe Celentano <giuseppecelentano4@gmail.com>
2024-06-20 17:56:23 +0200

    Add files via upload

---
+++ Esame2/src/esame2/Resizable.java
@@ -0,0 +1,11 @@
+package esame2;
+
+/**
+ *
+ * @author giuse
+ */
+public interface Resizable {
+    public void resize(int percentage) throws IllegalArgumentException;
+    public boolean checkQuality();
+    
+}
